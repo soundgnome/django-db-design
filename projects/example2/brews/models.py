5083053e7c357863e6d13c31a63448824edcc6a6
Terran Olson <terran@halfcadence.net>
2017-11-11 16:07:14 -0800

    adding example2

--- projects/example2/brews/models.py
+++ projects/example2/brews/models.py
@@ -1,15 +1,22 @@
-# models.py - example1
+# models.py - example2
 
 from django.db import models
 
 
-class Recipe(models.Model):
+class AbstractRecipe(models.Model):
     name = models.CharField(max_length=255)
     ingredients = models.TextField()
     directions = models.TextField()
 
+    class Meta:
+        abstract = True
 
-class Batch(Recipe):
+
+class Recipe(AbstractRecipe):
+    pass
+
+
+class Batch(AbstractRecipe):
     brew_date = models.DateField(blank=True, null=True)
     bottle_date = models.DateField(blank=True, null=True)
     notes = models.TextField(blank=True)
