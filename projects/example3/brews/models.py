02335b82dfd042053a6ab9461c98281d43aa5178
Terran Olson <terran@halfcadence.net>
2017-11-11 16:41:15 -0800

    adding example3

---
+++ projects/example3/brews/models.py
@@ -0,0 +1,29 @@
+# models.py - example3
+
+from django.db import models
+
+
+class AbstractRecipe(models.Model):
+    name = models.CharField(max_length=255)
+    directions = models.TextField()
+
+    class Meta:
+        abstract = True
+
+
+class Recipe(AbstractRecipe):
+    pass
+
+
+class Batch(AbstractRecipe):
+    brew_date = models.DateField(blank=True, null=True)
+    bottle_date = models.DateField(blank=True, null=True)
+    notes = models.TextField(blank=True)
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    ingredient_type = models.CharField(max_length=255)
+    amount = models.FloatField()
+    measuerment_unit = models.CharField(max_length=255)
